e04a717702af2103e6c221639bb00c7cb3c4a3e2
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-05-06 18:20:28 +0200

    Last changes

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,31 +5,24 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    New Task
+                    Nueva tarea
                 </div>
-
                 <div class="panel-body">
-                    <!-- Display Validation Errors -->
                     @include('common.errors')
-
-                    <!-- New Task Form -->
                     <form action="{{ url('task')}}" method="POST" class="form-horizontal">
                         {{ csrf_field() }}
 
-                        <!-- Task Name -->
                         <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
+                            <label for="task-name" class="col-sm-3 control-label">Título</label>
 
                             <div class="col-sm-6">
                                 <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
                             </div>
                         </div>
-
-                        <!-- Add Task Button -->
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Task
+                                    <i class="fa fa-btn fa-plus"></i>Añadir tarea
                                 </button>
                             </div>
                         </div>
@@ -37,16 +30,15 @@
                 </div>
             </div>
 
-            <!-- Current Tasks -->
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Current Tasks
+                        Tareas pendientes
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped task-table">
                             <thead>
-                                <th>Task</th>
+                                <th>Tarea</th>
                                 <th>&nbsp;</th>
                             </thead>
                             <tbody>
@@ -54,22 +46,19 @@
                                   @if($task->is_completed == 0)
                                     <tr>
                                         <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
                                         <td style="float:right;">
                                             <form action="{{ url('task/'.$task->id) }}" method="POST">
                                                 {{ csrf_field() }}
                                                 {{ method_field('DELETE') }}
 
                                                 <button type="submit" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                    <i class="fa fa-btn fa-trash"></i>Borrar
                                                 </button>
                                             </form>
                                             <form action="{{ url('task/update/'.$task->id) }}" method="GET">
                                               {{ csrf_field() }}
-
                                               <button type="submit" class="btn btn-success">
-                                                  <i class="fa fa-btn fa-check"></i>Done
+                                                  <i class="fa fa-btn fa-check"></i>Completada
                                               </button>
                                             </form>
                                         </td>
@@ -81,11 +70,10 @@
                     </div>
                 </div>
             @endif
-<!-- Completed Tasks -->
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Done Tasks
+                        Tareas completadas
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped task-table">
@@ -98,22 +86,19 @@
                                   @if($task->is_completed == 1)
                                     <tr>
                                         <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
                                         <td style="float:right;">
                                             <form action="{{ url('task/'.$task->id) }}" method="POST">
                                                 {{ csrf_field() }}
                                                 {{ method_field('DELETE') }}
 
                                                 <button type="submit" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                    <i class="fa fa-btn fa-trash"></i>Borrar
                                                 </button>
                                             </form>
                                             <form action="{{ url('task/update/'.$task->id) }}" method="GET">
                                               {{ csrf_field() }}
-
                                               <button type="submit" class="btn btn-warning">
-                                                <i class="fa fa-btn fa-undo"></i>Uncompleted
+                                                <i class="fa fa-btn fa-undo"></i>Marcar como pendiente
                                               </button>
                                             </form>
                                         </td>
